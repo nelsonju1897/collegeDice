cca99ab40bfe72e384b430675327c7b5a8945705
nelsonju1897 <nelsonju1897@gmail.com>
2019-10-15 10:24:41 -0500

    Adding individual values for dice rolls

--- 99DiceGame.js
+++ 99DiceGame.js
@@ -1,36 +1,96 @@
 function diceRoll(n){
-	return  Math.floor((Math.random() * n) + 1);
+return  Math.floor((Math.random() * n) + 1);
 }
 
-	let hundredSidedDice = diceRoll(100);
-	let twentySidedDice = diceRoll(20);	
-	let thirteenSidedDice = diceRoll(13);
-	let tenSidedDice = diceRoll(10);
-	let eightSidedDice = diceRoll(8);
-	let fourSidedDice = diceRoll(4);
-	let result = prompt("Please enter 100, 20, 13, 10, 8, or 4");
+function initialRoll(n){
 
-	if(result == "100"){
-		diceRoll(100);
-		console.log(hundredSidedDice)
+}
+		
+
+function runGame(){
+	// let hundredSidedDice;
+	// let twentySidedDice;
+	let thirteenSidedDice;
+	let tenSidedDice;
+	let eightSidedDice;
+	let fourSidedDice;
+
+	currentPlayerScore = diceRoll(100);
+	console.log(currentPlayerScore);
+
+	currentPlayerTurns = diceRoll(20);
+	console.log(currentPlayerTurns);	
+			
+
+	while(currentPlayerScore <= 99 && currentPlayerTurns > 0){
+		let result = prompt("Please enter 13, 10, 8, or 4");
+		
+		if(result == "13"){
+			thirteenSidedDice = diceRoll(1);
+
+				if(thirteenSidedDice == 1){
+					thirteenSidedDice++;
+					currentPlayerScore = currentPlayerScore + thirteenSidedDice
+				}
+				else if(thirteenSidedDice == 2){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 3){
+					currentPlayerTurns--;
+				}
+				else if(thirteenSidedDice == 4){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 5){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 6){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 7){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 8){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 9){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 10){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 11){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 12){
+					thirteenSidedDice++;
+				}
+				else if(thirteenSidedDice == 13){
+					thirteenSidedDice++;
+				}
+			// currentPlayerScore = currentPlayerScore + thirteenSidedDice
+			currentPlayerTurns--;
+			console.log("Your current score is:" + currentPlayerScore)
+			console.log(currentPlayerTurns)
+		}
+		else if(result == "10"){
+			
+			tenSidedDice = diceRoll(10);
+			console.log(tenSidedDice)
+		}
+		else if(result == "8"){
+			
+			eightSidedDice = diceRoll(8);
+			console.log(eightSidedDice)
+		}
+		else if(result == "4"){
+			
+			fourSidedDice = diceRoll(4);
+			console.log(fourSidedDice)
+		}
+		else{
+			console.log("Not valid");
+		}
+		
 	}
-	else if(result == "20"){
-		diceRoll(20);
-		console.log(twentySidedDice)
-	}
-	else if(result == "13"){
-		diceRoll(13);
-		console.log(thirteenSidedDice)
-	}
-	else if(result == "10"){
-		diceRoll(10);
-		console.log(tenSidedDice)
-	}
-	else if(result == "8"){
-		diceRoll(8);
-		console.log(eightSidedDice)
-	}
-	else if(result == "4"){
-		diceRoll(4);
-		console.log(fourSidedDice)
-	}
+}
